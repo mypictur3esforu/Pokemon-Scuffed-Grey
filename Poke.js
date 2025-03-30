fe860ca7af5a4f3fab5818a191b7b625839d76e9
Lukas Pribe <meink2220@gmail.com>
2025-03-30 19:00:40 +0200

    DB Connected
    Endlich Connection zur Datenbank + eine Funktion, die SQL befehle ausführen kann

---
+++ Poke.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql2');
+
+var mysql = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "2007",
+  database: "myDB"
+}).promise()
+
+async function executeSQL(sql) {
+  const [rows] = await mysql.query(sql)
+  return rows
+}
+
+console.log(executeSQL("select * from Pokemon"))
